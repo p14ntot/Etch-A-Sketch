3132fcbb092859b6b600c32954535a4741eba7fd
p14ntot <p14ntot@ionio.gr>
2023-03-17 12:16:06 +0200

    add first container and reset btn

---
+++ script.js
@@ -0,0 +1,91 @@
+const container=document.getElementById('main-container');
+
+
+// use the css function repeat function repeat() to create 16 rows and colums
+let num=16
+container.style.gridTemplateColumns='repeat(16,1fr)'
+container.style.gridTemplateRows='repeat(16,1fr)'
+
+for(let rows=0; rows<16; rows++){
+    
+    for(let colums=0; colums<16; colums++){
+
+        //create new divs
+        const newDiv=document.createElement('div');
+        newDiv.setAttribute('class', 'newDiv');
+        newDiv.style.border=' 1px solid #000'
+        container.appendChild(newDiv);
+
+        
+        
+    }
+}
+    
+
+
+// I put every item with class .newDiv in the neo
+const neo=document.querySelectorAll('.newDiv');
+
+// I use the for each to get access to every element
+neo.forEach(function(newDiv){
+    newDiv.addEventListener( "mouseover",function(){
+                newDiv.style.backgroundColor="#000"
+            })
+})
+
+function reset(){
+    const res=document.querySelectorAll('.newDiv');
+    res.forEach(function(newDiv){
+        
+                    newDiv.style.backgroundColor="white"
+                })
+    
+}
+    
+
+/*function change(){
+    let num=prompt('Give number of rows and colums');
+
+    const rem=document.querySelectorAll('.newDiv')
+    rem.forEach(function(newDiv){
+
+        container.removeChild(newDiv);
+    })
+
+
+    for(let rows=0; rows<num; rows++){
+    
+        for(let colums=0; colums<num; colums++){
+    
+            //create new divs
+            const newDiv=document.createElement('div');
+            newDiv.setAttribute('class', 'newDiv');
+            newDiv.style.border=' 1px solid #000'
+            container.appendChild(newDiv);
+    
+            
+            
+        }
+    }
+        
+    
+    
+    // I put every item with class .newDiv in the neo
+    const neo=document.querySelectorAll('.newDiv');
+    
+    // I use the for each to get access to every element
+    neo.forEach(function(newDiv){
+        newDiv.addEventListener( "mouseover",function(){
+                    newDiv.style.backgroundColor="#000"
+                })
+    })
+
+}*/
+    
+            
+            
+        
+        
+    
+    
+   
